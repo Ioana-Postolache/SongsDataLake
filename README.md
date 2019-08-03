7a41eb44011c4b57d0f79e8161c00e63994322d1
Ioana-Postolache <ioana.postolache.zaharia@gmail.com>
2019-08-03 11:59:13 +0000

    Started working on songs_data - created songs and artists table

---
+++ README.md
@@ -0,0 +1,21 @@
++------------------+---------------+-----------------+----------------+----------------------------------------------------------------------------------------------+---------+---------+------------------+----------------------------------------------------+----+
+|artist_id         |artist_latitude|artist_location  |artist_longitude|artist_name                                                                                   |duration |num_songs|song_id |title                                               |year|
++------------------+---------------+-----------------+----------------+----------------------------------------------------------------------------------------------+---------+---------+------------------+----------------------------------------------------+----+
+|ARDR4AC1187FB371A1|null           |                 |null            |Montserrat Caballé;Placido Domingo;Vicente Sardinero;Judith Blegen;Sherrill Milnes;Georg Solti|511.16363|1        |SOBAYLL12A8C138AF9|Sono andati? Fingevo di dormire                     |0   |
+|AREBBGV1187FB523D2|null           |Houston, TX      |null            |Mike Jones (Featuring CJ_ Mello & Lil' Bran)                                                  |173.66159|1        |SOOLYAZ12A6701F4A6|Laws Patrolling (Album Version)                     |0   |
+|ARMAC4T1187FB3FA4C|40.82624       |Morris Plains, NJ|-74.47995       |The Dillinger Escape Plan                                                                     |207.77751|1        |SOBBUGU12A8C13E95D|Setting Fire to Sleeping Giants                     |2004|
+|ARPBNLO1187FB3D52F|40.71455       |New York, NY     |-74.00712       |Tiny Tim                                                                                      |43.36281 |1        |SOAOIBZ12AB01815BE|I Hold Your Hand In Mine [Live At Royal Albert Hall]|2000|
+|ARDNS031187B9924F0|32.67828       |Georgia          |-83.22295       |Tim Wilson                                                                                    |186.48771|1        |SONYPOM12A8C13B2D7|I Think My Wife Is Running Around On Me (Taco Hell) |2005|
++------------------+---------------+-----------------+----------------+----------------------------------------------------------------------------------------------+---------+---------+------------------+----------------------------------------------------+----+
+only showing top 5 rows
+
+#  |-- artist_id:  (nullable = true)
+#  |-- artist_latitude: double (nullable = true)
+#  |-- artist_location: string (nullable = true)
+#  |-- artist_longitude: double (nullable = true)
+#  |-- artist_name: string (nullable = true)
+#  |-- duration: double (nullable = true)
+#  |-- num_songs: long (nullable = true)
+#  |-- song_id: string (nullable = true)
+#  |-- title: string (nullable = true)
+#  |-- year: long (nullable = true)
--- etl.py
+++ etl.py
@@ -4,13 +4,14 @@ import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.types import StructType as R, StructField as Fld, DoubleType as Dbl, StringType as Str, IntegerType as Int, DateType as Date
 
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -23,71 +24,110 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
-    
+    song_data = os.path.join(input_data, "song_data/A/*/*/*.json")
+    print(song_data)
+
     # read song data file
-    df = 
+    songsSchema = R([
+    Fld("artist_id",Str()),
+    Fld("artist_latitude",Dbl()),
+    Fld("artist_location",Str()),
+    Fld("artist_longitude",Dbl()),
+    Fld("artist_name",Str()),
+    Fld("duration",Dbl()),
+    Fld("num_songs",Int()),
+    Fld("song_id",Str()),    
+    Fld("title",Str()),    
+    Fld("year",Int()),
+    
+])
+
+    df = spark.read.json(song_data, schema=songsSchema)
+    print(df.count())
+    print(df.show(5, truncate=False))
+
+    df.printSchema()
+    
+#  |-- artist_id: string (nullable = true)
+#  |-- artist_latitude: double (nullable = true)
+#  |-- artist_location: string (nullable = true)
+#  |-- artist_longitude: double (nullable = true)
+#  |-- artist_name: string (nullable = true)
+#  |-- duration: double (nullable = true)
+#  |-- num_songs: long (nullable = true)
+#  |-- song_id: string (nullable = true)
+#  |-- title: string (nullable = true)
+#  |-- year: long (nullable = true)
+
 
     # extract columns to create songs table
-    songs_table = 
     
-    # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table = df.select("song_id", "title", "artist_id", "year", "duration")
+    songs_table.printSchema()
+    songs_table.show(5)
+    
+     # write songs table to parquet files partitioned by year and artist
+    songs_table = songs_table.write.mode('overwrite').partitionBy("year", "artist_id").parquet(output_data + "songs")
 
     # extract columns to create artists table
-    artists_table = 
+#     artists_table = df.select("artist_id", "name", "location", "lattitude", "longitude")
+    df.createOrReplaceTempView("df")
+    artists_table = spark.sql("select artist_id, artist_name as name, artist_location as location, artist_latitude as latitude, artist_longitude as longitude from df")
+    artists_table.printSchema()
+    artists_table.show(5)
+
+#     # write artists table to parquet files
+#     artists_table
+
+
+# def process_log_data(spark, input_data, output_data):
+#     # get filepath to log data file
+#     log_data =
+
+#     # read log data file
+#     df = 
     
-    # write artists table to parquet files
-    artists_table
+#     # filter by actions for song plays
+#     df = 
 
-
-def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data =
-
-    # read log data file
-    df = 
+#     # extract columns for users table    
+#     artists_table = 
     
-    # filter by actions for song plays
-    df = 
+#     # write users table to parquet files
+#     artists_table
 
-    # extract columns for users table    
-    artists_table = 
+#     # create timestamp column from original timestamp column
+#     get_timestamp = udf()
+#     df = 
     
-    # write users table to parquet files
-    artists_table
-
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
+#     # create datetime column from original timestamp column
+#     get_datetime = udf()
+#     df = 
     
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+#     # extract columns to create time table
+#     time_table = 
     
-    # extract columns to create time table
-    time_table = 
-    
-    # write time table to parquet files partitioned by year and month
-    time_table
+#     # write time table to parquet files partitioned by year and month
+#     time_table
 
-    # read in song data to use for songplays table
-    song_df = 
+#     # read in song data to use for songplays table
+#     song_df = 
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+#     # extract columns from joined song and log datasets to create songplays table 
+#     songplays_table = 
 
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
+#     # write songplays table to parquet files partitioned by year and month
+#     songplays_table
 
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
+#     input_data = "s3a://udacity-dend/"
+    input_data = ""
+    output_data = "s3a//songs-data-lake/"
     
     process_song_data(spark, input_data, output_data)    
-    process_log_data(spark, input_data, output_data)
+#     process_log_data(spark, input_data, output_data)
 
 
 if __name__ == "__main__":
